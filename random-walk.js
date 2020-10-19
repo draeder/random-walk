efadd820844e2d322f6b6bd8e7c193235692d344
draeder <draeder@gmail.com>
2020-10-19 06:52:21 -0500

    updates

--- random-walk.js
+++ random-walk.js
@@ -69,18 +69,7 @@ var boxMullerRandom = (function () {
     }
 
     return function () {
-        if (!phase) {
-            do {
-                x1 = 2.0 * random() - 1.0;
-                x2 = 2.0 * random() - 1.0;
-                w = x1 * x1 + x2 * x2;
-            } while (w >= 1.0);
 
-            w = Math.sqrt((-2.0 * Math.log(w)) / w);
-            z = x1 * w;
-        } else {
-            z = x2 * w;
-        }
 
         phase ^= 1;
         //console.log(z)
